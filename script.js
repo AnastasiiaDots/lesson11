268213b94db237752514944f7fc1683f917056e5
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-02-22 20:49:45 +1300

    lesson11 task 2 added

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const btn = document.getElementById('btn');
 const square = document.getElementById('square');
 const text = document.getElementById('text');
 
-const colors = ['red', 'blue', 'green', 'yellow', 'purple'];
+const e_btn = document.getElementById('e_btn');
 
-
+e_btn.style.display = 'none';
 
 btn.addEventListener('click', () => {
     const color = text.value;
     text.textContent = color;
     square.style.backgroundColor = color;
 });
+
+
 
 
 console.dir(square);
